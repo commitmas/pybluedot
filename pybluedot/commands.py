dedd4097e72b0d2111b4f695cd989f1b749f501a
Matt Oswalt <matt@keepingitclassless.net>
2016-12-05 13:37:45 -0800

    Cleaned up commands.py a bit
    Signed-off-by: Matt Oswalt <matt@keepingitclassless.net>

--- pybluedot/commands.py
+++ pybluedot/commands.py
@@ -1,45 +1,41 @@
-"""
-commands.py contains the data structures that cli.py uses to construct the arguments/subcommand structure. 
+""" commands.py contains the data structures that cli.py uses to construct the arguments/subcommand structure. 
 """
 
-"""
-cmd_arg: global arguments and flags to the command, like '--verbose' or '--debug'
-"""
-cmd_arg = '[
-            {
-                name: "verbose",
-                action: "store_const"
-            },
-            {
-                name: "debug",
-                action: "store_const"
-            }
-            ]'
+# global arguments and flags to the command, like '--verbose' or '--debug'
+cmd_arg = [
+    {
+        name: "verbose",
+        action: "store_const"
+    },
+    {
+        name: "debug",
+        action: "store_const"
+    }
+]
 
-"""
-sub_cmd: subcommands used by the program, like 'bdot init' or 'bdot search'.  Subcommands are tied to functions, so you must specify a function to use with each subcommand.
-"""
-sub_cmd = '[
-            {
-                name: "init",
-                help: "create a pybluedot configuration file",
-                options[
-                        {
-                            name: "--force",
-                            action: "store_const"
-                        },
-                        {
-                            name: "--display",
-                            action: "store_const"
-                        }
-                    ],
-                function: "init"
-            },
-            {
-                name: "sounds",
-                help: "just useless boilerplate atm!",
-                options[],
-                function: "sounds"
-            }
-        ]'
+# subcommands used by the program, like 'bdot init' or 'bdot search'.
+# Subcommands are tied to functions, so you must specify a function to use with each subcommand.
+sub_cmd = [
+    {
+        name: "init",
+        help: "create a pybluedot configuration file",
+        options: [
+                {
+                    name: "--force",
+                    action: "store_const"
+                },
+                {
+                    name: "--display",
+                    action: "store_const"
+                }
+            ],
+        function: "init"
+    },
+    {
+        name: "sounds",
+        help: "just useless boilerplate atm!",
+        options: [],
+        function: "sounds"
+    }
+]
 
