f7dfe44959de095e068bb2be27dd4a6ccc4b1ffe
jeorryb <jeorry@gmail.com>
2016-12-06 10:13:36 -0500

    Add argparse section

--- patent.py
+++ patent.py
@@ -1,16 +1,36 @@
+import argparse
 import requests
+import json
 
-patent_url = 'https://api.nasa.gov/patents/content'
 
-#api key obtained from NASA
-my_api_key = 'aApglMV0QO74TpuZACRnSUvGel8ayxf7Z067Nnsa'
-query = 'plastic'
-limit = '10'
-con_tags = False
+def patent(api_key, query, concept_tags, limit):
+    patent_url = 'https://api.nasa.gov/patents/content'
+    get_params = {
+        'query': query,
+        'concept_tags': concept_tags,
+        'limit': limit,
+        'api_key': api_key,
+    }
+    get_response = requests.get(patent_url, params = get_params)
 
-#NASA Patents query parameters
-get_params = {'query': query, 'limit': limit, 'api_key': my_api_key}
+    if get_response.status_code == 429:
+        raise Exception('You have exceeded your rate limit')
 
-getRequest = requests.get(patent_url, params = get_params)
+    body = get_response.json()
 
-print getRequest.json()
+    if 'error' in body:
+        raise Exception(body['error'])
+# Current default is to print out json, need to add ability for file output
+    print(json.dumps(body, indent=2))
+
+
+def main():
+    ap = argparse.ArgumentParser(description = 'Search NASA patent portfolio')
+    ap.add_argument('-a', '--api_key',
+                    help = 'Your NASA API key', required=True)
+    ap.add_argument('q', '--query', help = 'Text to search on', required=True)
+    ap.add_argument('c', '--concept_tags',
+                    help = 'Return ordered dict of concepts from the abstract')
+    ap.add_argument('l', '--limit', help = 'Number of patents to return',
+                   type=int)
+    
